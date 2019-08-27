a6c6448ce953d1381c509721e665fc6afb242594
jwvictory <joseph.victory@ladbrokescoral.com>
2019-08-27 16:28:05 +0200

    no message

---
+++ content/blog/autocomplete-css-properties/index.md
@@ -0,0 +1,23 @@
+---
+title: CSS - Autocomplete CSS Properties
+date: "2019-08-27"
+description: "CSS - Autocomplete CSS Properties"
+---
+
+Can't remember a CSS property name but know the value you need?
+
+Chrome DevTools has you covered!
+
+Within the element.style section in DevTools start typing the value you want to use. If recognised, DevTools with create auto suggestions saving you time.
+
+Example:
+
+1. Type 'italic' into the element.style section.
+2. DevTools will auto suggest: 
+```css
+font-style:italic
+```
+3. Magic.
+
+
+
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `DevTool Tips`,
+    author: `Bob Cool`,
+    description: `Dev Tool Tips is a collection of Chrome Developer Tool tips learnt over time.`,
+    siteUrl: `https://devtooltips.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `#`,
     },
   },
   plugins: [
