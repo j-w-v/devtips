24ca82a35e4735da472b1f470b32bcd778a6fcb0
jwvictory <joseph.victory@ladbrokescoral.com>
2019-08-29 10:30:54 +0200

    no message

--- src/components/form.js
+++ src/components/form.js
@@ -4,7 +4,10 @@ const Form = () => {
   return (
       <>
     <p>Want more tips? Get an email each time we post a new one.</p>
-    <form name="Contact Form" method="POST" data-netlify="true" action="/thank-you" enctype="application/x-www-form-urlencoded">
+    <form name="Contact Form" method="POST" data-netlify="true" netlify-honeypot="bot-field" action="/thank-you" enctype="application/x-www-form-urlencoded">
+    <p class="hidden">
+    <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+  </p>
       <p>
         <label>
           Name <input type="text" name="name" />
